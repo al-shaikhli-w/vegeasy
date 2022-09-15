d4ae50720a2ff9a60cfd45275b170258f3866380
Waleed Al-Shaikhli <Waleedkaleed24@gmail.com>
2022-09-15 13:07:55 +0200

    add the favicons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+// import favicons
+import "./image/favicon.ico";
+import "./image/apple-touch-icon.png";
+import "./image/android-chrome-192x192.png";
+import "./image/android-chrome-512x512.png";
+import "./image/favicon-16x16.png";
+import "./image/favicon-32x32.png";
+
+// import scss files
 import "./scss/main.scss";
 
 // import js files
 import "./js/index";
-
